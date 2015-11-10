77cc2eb86f3c5728244eda982a64af7225a9d694
smartapant <smartapant@gmail.com>
2015-11-10 14:23:32 +0300

    Postrow to posts dir

--- app/views/posts/index.ios.js
+++ app/views/posts/index.ios.js
@@ -5,6 +5,8 @@ var {
   Text,
   View,
   ListView,
+  Image,
+  Dimensions,
   TouchableHighlight,
   } = React;
 
@@ -12,7 +14,83 @@ var InfiniteScrollView = require('react-native-infinite-scroll-view');
 
 var styles = require('./styles');
 var redditApi = require('../../api/reddit');
-var PostRow = require('./elements/postRow');
+var Post = require('../post');
+var Comments = require('../comments');
+
+var commentsIcon = require('image!comments');
+
+var PostRow = React.createClass({
+  render: function() {
+    var thumbnail = this.props.post.preview && this.props.post.preview.images[0].source;
+    var imageEl = this._getImageElement(thumbnail);
+    return (
+      <View style={styles.container}>
+        <TouchableHighlight onPress={this.showPost}>
+          <View style={styles.container}>
+            <Text style={styles.postTitle}>
+              {this.props.post.title}
+            </Text>
+            {imageEl}
+            <View style={styles.postStats} onPress={this.showComments}>
+              <Text style={styles.statsTextStyles}>{this.props.post.score || 0} points</Text>
+              <View style={styles.dotDelimeter}><Text style={styles.statsTextStyles}> . </Text></View>
+              <Text style={styles.statsTextStyles}>{this.props.post.num_comments || 0} comments</Text>
+              <View style={styles.dotDelimeter}><Text style={styles.statsTextStyles}> . </Text></View>
+              <Text style={[styles.statsTextStyles, styles.blue]}>{'/r/' + this.props.post.subreddit}</Text>
+              <View style={styles.dotDelimeter}><Text style={styles.statsTextStyles}> . </Text></View>
+              <Text style={[styles.statsTextStyles, styles.blue]}>{this.props.post.author}</Text>
+            </View>
+          </View>
+        </TouchableHighlight>
+        <TouchableHighlight onPress={this.showComments}>
+          <View style={[styles.container, styles.commentsSection]} onPress={this.showComments}>
+            <Image
+              style={styles.commentsIcon}
+              source={commentsIcon}
+              />
+            <View style={styles.readComments}>
+              <Text style={[styles.statsTextStyles]}>  Read comments</Text>
+            </View>
+          </View>
+        </TouchableHighlight>
+        <View style={styles.separator}/>
+      </View>
+    );
+  },
+
+  showPost: function () {
+    this.props.navigator.push({
+      title: this.props.post.title,
+      component: Post,
+      passProps: {url: this.props.post.url}
+    });
+  },
+
+  showComments: function () {
+    this.props.navigator.push({
+      title: 'Comments - ' + this.props.post.title,
+      component: Comments,
+      passProps: {post: this.props.post}
+    });
+  },
+
+  _getImageElement: function (image) {
+    return image ?
+      ( <View style={styles.imageContainer}>
+        <Image
+          style={this._getImageStyle(image)}
+          source={{uri: image.url}}
+          />
+      </View>) :
+      ( <View/>);
+  },
+
+  _getImageStyle: function (image) {
+    var width = Dimensions.get('window').width;
+    var height = image.height * width / image.width;
+    return {width: width, height: height};
+  }
+});
 
 var PostsView = React.createClass({
 
--- app/views/posts/styles.js
+++ app/views/posts/styles.js
@@ -2,13 +2,76 @@ var React = require('react-native');
 
 var {
   StyleSheet,
+  PixelRatio
   } = React;
 
 module.exports = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+  },
   text: {
     color: 'black',
     backgroundColor: 'white',
     fontSize: 30,
     margin: 80
-  }
+  },
+  imageContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'center'
+  },
+  postTitle: {
+    fontSize: 15,
+    textAlign: 'left',
+    marginTop: 10,
+    marginBottom: 4,
+    marginLeft: 5,
+    fontWeight: 'bold',
+    color: '#444444'
+  },
+  postStats: {
+    marginTop: 6,
+    marginLeft: 5,
+    flexDirection: 'row'
+  },
+  statsTextStyles: {
+    color: 'lightslategray',
+    fontSize: 10,
+    textAlign: 'left'
+  },
+  commentsSection: {
+    paddingBottom: 5,
+    paddingTop: 5,
+    paddingLeft: 4,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'flex-start'
+  },
+  commentsIcon: {
+    width: 15,
+    height: 15,
+    opacity: 0.3,
+  },
+  readComments: {
+    position: 'relative',
+    bottom: 2
+  },
+  blue: {
+    color: '#48BBEC',
+    fontWeight: 'bold'
+  },
+  dotDelimeter: {
+    position: 'relative',
+    bottom: 2
+  },
+  dotDelimeterText: {
+    color: 'lightslategray',
+    fontSize: 20,
+    fontWeight: 'bold'
+  },
+  separator: {
+    height: 1/PixelRatio.get(),
+    backgroundColor: '#CACACA',
+  },
 });
